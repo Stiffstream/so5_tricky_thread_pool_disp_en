c714dd1c5b323365085c4b8d981f90d67b89ccc4
Yauheni Akhotnikau <eao197@gmail.com>
2024-02-03 10:39:00 +0300

    Update of dependencies.
    SObjectizer -> 5.8.1.1.
    fmtlib -> 10.2.1.

--- externals.rb
+++ externals.rb
@@ -1,5 +1,5 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.0.tar.gz'
+  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.1.1.tar.gz'
 
   e.map_dir 'dev/so_5' => 'dev'
 end
@@ -11,14 +11,14 @@ MxxRu::arch_externals :clara do |e|
 end
 
 MxxRu::arch_externals :fmt do |e|
-  e.url 'https://github.com/fmtlib/fmt/releases/download/10.0.0/fmt-10.0.0.zip'
+  e.url 'https://github.com/fmtlib/fmt/releases/download/10.2.1/fmt-10.2.1.zip'
 
   e.map_dir 'include' => 'dev/fmt'
   e.map_dir 'src' => 'dev/fmt'
   e.map_dir 'support' => 'dev/fmt'
   e.map_file 'CMakeLists.txt' => 'dev/fmt/*'
-  e.map_file 'README.rst' => 'dev/fmt/*'
-  e.map_file 'ChangeLog.rst' => 'dev/fmt/*'
+  e.map_file 'README.md' => 'dev/fmt/*'
+  e.map_file 'ChangeLog.md' => 'dev/fmt/*'
 end
 
 MxxRu::arch_externals :fmtlib_mxxru do |e|
